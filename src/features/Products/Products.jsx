ed3d72831e9a1fdceca2c73a073ed9af091965d9
Pius <piusdonkor1@gmail.com>
2024-08-13 04:48:05 +0000

    Create Products.jsx

---
+++ src/features/Products/Products.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable react/prop-types */
+import ProductCard from "./ProductCard";
+
+export default function Products({ products }) {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+      {products.map((product) => (
+        <ProductCard product={product} key={product.product_id} />
+      ))}
+    </div>
+  );
+}
