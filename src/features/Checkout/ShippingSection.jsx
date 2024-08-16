37259e279de98ba4913afff36e49d55bbd9f4f6b
Pius <piusdonkor1@gmail.com>
2024-08-16 00:33:24 +0000

    Update ShippingSection.jsx

--- src/features/Checkout/ShippingSection.jsx
+++ src/features/Checkout/ShippingSection.jsx
@@ -2,7 +2,11 @@
 import { useForm } from "react-hook-form";
 import Button from "../../ui/Button";
 
-export default function ShippingSection({ onNextStep, setShippingDetails }) {
+export default function ShippingSection({
+  onNextStep,
+  setShippingDetails,
+  cartItems = [],
+}) {
   const {
     register,
     handleSubmit,
@@ -20,6 +24,10 @@ export default function ShippingSection({ onNextStep, setShippingDetails }) {
     reset();
     onNextStep(); // Move to the next step
   };
+  const subtotal = cartItems.reduce(
+    (acc, item) => acc + item.price * item.quantity,
+    0
+  );
 
   const selectedPaymentMethod = watch("paymentMethod", "Credit Card");
 
@@ -193,9 +201,9 @@ export default function ShippingSection({ onNextStep, setShippingDetails }) {
 
         <div className="bg-[#fff] h-fit shadow-md rounded-lg p-4 sticky top-20">
           <h3 className="text-xl font-semibold mb-4">Order Summary</h3>
-          <p className="text-lg">Subtotal: $300</p>
-          <p className="text-lg">Shipping: $20</p>
-          <p className="text-lg font-bold">Total: $320</p>
+          <p className="text-lg">Subtotal: &#8373;{subtotal}</p>
+          <p className="text-lg">Shipping: &#8373;20</p>
+          <p className="text-lg font-bold">Total: &#8373;{subtotal + 20}</p>
         </div>
       </div>
     </div>
