5b184d3d2e0e53f19f39ea45659b4cbc17c166f6
Pius <piusdonkor1@gmail.com>
2024-08-19 23:00:29 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,39 +11,57 @@ import { FaCartArrowDown } from "react-icons/fa6";
 import OrdersPage from "./pages/OrdersPage";
 import LoginPage from "./pages/LoginPage";
 import SignupPage from "./pages/SignupPage";
+import ProtectedRoute from "./features/Authentication/ProtectedRoute";
+import { AuthenticationProvider } from "./context/AuthContext";
 
 function App() {
   return (
-    <CartProvider>
-      <Toaster
-        toastOptions={{
-          style: {
-            zIndex: 9999,
-          },
-          success: {
-            icon: (
-              <p className="p-2 bg-[#82e0c4c2] rounded-full ">
-                <FaCartArrowDown className="text-2xl text-emeraldGreen " />
-              </p>
-            ),
-          },
-        }}
-      />
-      <Router>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<HomePage />} />
-            <Route path="/cart" element={<CartPage />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/signup" element={<SignupPage />} />
-            <Route path="/orders" element={<OrdersPage />} />
-            <Route path="/checkoutPage" element={<CheckoutPage />} />
-            <Route path="/product/:id" element={<ProductDetailsPage />} />
-            <Route path="/products" element={<ProductsPage />} />
-          </Route>
-        </Routes>
-      </Router>
-    </CartProvider>
+    <AuthenticationProvider>
+      <CartProvider>
+        <Toaster
+          toastOptions={{
+            style: {
+              zIndex: 9999,
+            },
+            success: {
+              icon: (
+                <p className="p-2 bg-[#82e0c4c2] rounded-full ">
+                  <FaCartArrowDown className="text-2xl text-emeraldGreen " />
+                </p>
+              ),
+            },
+          }}
+        />
+        <Router>
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route index element={<HomePage />} />
+              <Route path="/cart" element={<CartPage />} />
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/signup" element={<SignupPage />} />
+              <Route
+                path="/orders"
+                element={
+                  <ProtectedRoute>
+                    <OrdersPage />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/checkoutPage"
+                element={
+                  <ProtectedRoute>
+                    <CheckoutPage />
+                  </ProtectedRoute>
+                }
+              />
+              <Route path="/product/:id" element={<ProductDetailsPage />} />
+              <Route path="/products" element={<ProductsPage />} />
+            </Route>
+          </Routes>
+        </Router>
+      </CartProvider>
+    </AuthenticationProvider>
   );
 }
 
